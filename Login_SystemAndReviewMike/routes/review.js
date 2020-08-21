9aba5447c7266331f28956a20acc9e8e6f2df954
Zack Garrison <fezzick217@gmail.com>
2020-08-21 12:05:41 -0400

    Goodbye junk
    goodbye junk

--- Login_SystemAndReviewMike/routes/review.js
+++
@@ -1,8 +0,0 @@
-const express= require('express');
-const reviewController= require('../controllers/review');
-const router = express.Router();
-
-router.post("/trailreview", reviewController.trailreview);
-   
-
-module.exports= router;
